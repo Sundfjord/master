3850d4dbfb31b625a9e3ec59bed84d5192ad8cab
Yngve Sundfjord <yngvesundfjord@gmail.com>
2013-10-18 15:25:47 +0200

    Allowed space in username and added "Tankstrapped" reset_password_form

--- application/views/auth/reset_password_form.php
+++ application/views/auth/reset_password_form.php
@@ -1,4 +1,6 @@
 <?php
+$this->config->load('tankstrap'); 
+$tankstrap = $this->config->item('tankstrap');
 $new_password = array(
 	'name'	=> 'new_password',
 	'id'	=> 'new_password',
@@ -12,18 +14,43 @@ $confirm_new_password = array(
 	'size' 	=> 30,
 );
 ?>
-<?php echo form_open($this->uri->uri_string()); ?>
-<table>
-	<tr>
-		<td><?php echo form_label('New Password', $new_password['id']); ?></td>
-		<td><?php echo form_password($new_password); ?></td>
-		<td style="color: red;"><?php echo form_error($new_password['name']); ?><?php echo isset($errors[$new_password['name']])?$errors[$new_password['name']]:''; ?></td>
-	</tr>
-	<tr>
-		<td><?php echo form_label('Confirm New Password', $confirm_new_password['id']); ?></td>
-		<td><?php echo form_password($confirm_new_password); ?></td>
-		<td style="color: red;"><?php echo form_error($confirm_new_password['name']); ?><?php echo isset($errors[$confirm_new_password['name']])?$errors[$confirm_new_password['name']]:''; ?></td>
-	</tr>
-</table>
-<?php echo form_submit('change', 'Change Password'); ?>
-<?php echo form_close(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="<?php echo $tankstrap["bootstrap_path"];?>" rel="stylesheet">
+        <title><?php echo $tankstrap["new_pw_page_title"];?></title>
+    </head>
+    <body>
+	<div class="container">
+            <div class="row">
+                <div class="span6 offset3">
+                    <div class="well">
+                        <center>
+                            <h2>New Password</h2>
+                                    <?php echo form_open($this->uri->uri_string()); ?>
+                            <div class="control-group">
+                                    <?php echo form_label('New Password', $new_password['id']); ?>
+                                <div class="controls">
+                                    <?php echo form_password($new_password); ?>
+                                    <?php echo form_error($new_password['name']); ?><?php echo isset($errors[$new_password['name']])?$errors[$new_password['name']]:''; ?>
+                                </div>
+                            </div>
+                            
+                            <div class="control-group">
+                                    <?php echo form_label('Confirm New Password', $confirm_new_password['id']); ?>
+                                <div class="controls">        
+                                    <?php echo form_password($confirm_new_password); ?>
+                                    <?php echo form_error($confirm_new_password['name']); ?><?php echo isset($errors[$confirm_new_password['name']])?$errors[$confirm_new_password['name']]:''; ?>
+                                    <p class="help-block"></p>
+                                </div>
+                            </div>
+                            <?php echo form_submit('change', 'Change Password', 'class="btn btn-primary"'); ?>
+                            <?php echo form_close(); ?>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
