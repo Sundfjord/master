f573447f3ab838b13247913e87e6464a6f049f64
Yngve Sundfjord <yngvesundfjord@gmail.com>
2013-10-22 09:30:06 +0200

    Users selecting Coach upon reg is regarded admin

--- application/views/auth/register_form.php
+++ application/views/auth/register_form.php
@@ -1,40 +1,48 @@
 <?php
 $this->config->load('tankstrap'); 
 $tankstrap = $this->config->item('tankstrap');
+
 if ($use_username) {
 	$username = array(
-		'name'	=> 'username',
-		'id'	=> 'username',
-		'value' => set_value('username'),
-		'maxlength'	=> $this->config->item('username_max_length', 'tank_auth'),
-		'size'	=> 30,
+		'name'      => 'username',
+		'id'        => 'username',
+		'value'     => set_value('username'),
+		'maxlength' => $this->config->item('username_max_length', 'tank_auth'),
+		'size'      => 30,
 	);
 }
 $email = array(
-	'name'	=> 'email',
-	'id'	=> 'email',
-	'value'	=> set_value('email'),
-	'maxlength'	=> 80,
-	'size'	=> 30,
+	'name'      => 'email',
+	'id'        => 'email',
+	'value'     => set_value('email'),
+	'maxlength' => 80,
+	'size'      => 30,
 );
+
+$group_id = array(
+        ''          => 'Select role',
+        '300'       => 'Player',
+        '100'       => 'Coach'
+);
+
 $password = array(
-	'name'	=> 'password',
-	'id'	=> 'password',
-	'value' => set_value('password'),
-	'maxlength'	=> $this->config->item('password_max_length', 'tank_auth'),
-	'size'	=> 30,
+	'name'      => 'password',
+	'id'        => 'password',
+	'value'     => set_value('password'),
+	'maxlength' => $this->config->item('password_max_length', 'tank_auth'),
+	'size'      => 30,
 );
 $confirm_password = array(
-	'name'	=> 'confirm_password',
-	'id'	=> 'confirm_password',
-	'value' => set_value('confirm_password'),
-	'maxlength'	=> $this->config->item('password_max_length', 'tank_auth'),
-	'size'	=> 30,
+	'name'      => 'confirm_password',
+	'id'        => 'confirm_password',
+	'value'     => set_value('confirm_password'),
+	'maxlength' => $this->config->item('password_max_length', 'tank_auth'),
+	'size'      => 30,
 );
 $captcha = array(
-	'name'	=> 'captcha',
-	'id'	=> 'captcha',
-	'maxlength'	=> 8,
+	'name'      => 'captcha',
+	'id'        => 'captcha',
+	'maxlength' => 8,
 );
 ?>
 <!DOCTYPE html>
@@ -49,7 +57,7 @@ $captcha = array(
 			<div class="span6 offset3">
 				<div class="well">
 					<center>
-					<h2>Register!</h2>
+					<h2>Register</h2>
 <?php echo form_open($this->uri->uri_string()); ?>
 
 	<?php if ($use_username): ?>
@@ -70,6 +78,15 @@ $captcha = array(
             <p class="help-block"></p>
         </div>
     </div>
+                                        
+    <div class="control-group">
+        <p> Role <!-- echo form_label('Role', $group_id['group_id'], array('class' =>'control-label')); ? --></p>                                  
+        <div class="controls">
+            <?php echo form_error('role'); ?>
+            <?php echo form_dropdown('group_id', $group_id, 'role'); ?> <br />
+        </div>
+    </div>                                    
+                                        
     <div class="control-group">
         <?php echo form_label('Password', $password['id'], array('class' => 'control-label')); ?>
         <div class="controls">
