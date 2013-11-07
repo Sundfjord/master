146286d5ce38c8eda7d94cc55ff7fd3419a16897
Yngve Sundfjord <yngvesundfjord@gmail.com>
2013-11-07 17:40:37 +0100

    minor or no changes

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,6 +1,6 @@
-<ul>
+<!-- <ul>
     <li><a href="<?php echo base_url(''); ?>">About</a></li>
     <li><a href="<?php echo base_url(''); ?>">Privacy</a></li>
     <li><a href="http://sundfjord.com/">Sundfjord</a></li>
-</ul>
+</ul> -->
 
--- application/views/includes/template.php
+++ application/views/includes/template.php
@@ -7,10 +7,10 @@
         <meta name="robots" content="noindex">
         <title><?php echo $title; ?></title>
         <script src="<?php echo base_url(); ?>js/jquery.js" type="text/javascript" charset="utf-8"></script>
-        <script src="<?php echo base_url(); ?>js/functions.js" type="text/javascript" charset="utf-8"></script>
         <script src="<?php echo base_url(); ?>js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
-        <link rel="stylesheet" href="<?php echo base_url();?>/css/bootstrap.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" />
+        <script src="<?php echo base_url(); ?>js/functions.js" type="text/javascript" charset="utf-8"></script>
+        <link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" />
     </head>
     
     <body>
@@ -18,18 +18,18 @@
             <div id="header">
                 <?php $this->load->view('includes/header'); ?>
             </div><!--header-->
-               
-            <div id="sidebar">
-                <?php $this->load->view('includes/sidebar'); ?>
-            </div><!--sidebar-->
             
-            <div id="content">
-                <?php $this->load->view($main_content); ?>
-            </div><!--Content-->
+                <div id="sidebar">
+                    <?php $this->load->view('includes/sidebar'); ?>
+                </div><!--sidebar-->
+
+                <div id="content"
+                    <?php $this->load->view($main_content); ?>
+                </div><!--Content-->
 
             <div id="footer">
                 <?php $this->load->view('includes/footer'); ?>
-            </div><!--sidebar-->
+            </div><!--footer-->
 
         </div><!--Wrapper-->
     </body>
