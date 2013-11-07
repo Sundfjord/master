61569db90c815ed6c0e3b60a765338c8e8749adb
Yngve Sundfjord <yngvesundfjord@gmail.com>
2013-11-07 17:42:38 +0100

    changed style and functionality of sidebar

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -1,31 +1,31 @@
 <?php if ( $coach === TRUE ) : ?>
-                
-<ul id="menu" class="nav nav-tabs nav-stacked">
-    <li class="active" data-toggle="collapse" data-target="#submenu"><a href="#"><i class="icon-list"></i>My Teams<i style="float: right;"class="icon-chevron-down"></i></a></li>
+
+<ul id="menu" class="nav nav-pills nav-stacked">
+    <li data-toggle="collapse" data-target="#submenu"><a href="#"><span class="glyphicon glyphicon-list"></span>My Teams<span style="float:right; "class="glyphicon glyphicon-chevron-down"></span></a></li>
         <ul id="submenu" class="nav nav-list collapse">
-            <li><a href="#">Epic United</a></li>
-            <li><a href="#">Awesome Rovers</a></li>
-            <li><a href="#">Boston Fuckups</a></li>
-            <li><a data-toggle="modal" id="create_team" data-backdrop="true" href="#create_team_modal" href="<?php echo base_url('index.php/team/create_team'); ?>"><i class="icon-plus"></i>Create a team</a></li> 
+            <li><a href="<?php echo base_url(); ?>">Epic United</a></li>
+            <li><a href="<?php echo base_url(); ?>">Awesome Rovers</a></li>
+            <li><a href="<?php echo base_url(); ?>">Boston Fuckups</a></li>
+            <li><a data-toggle="modal" id="create_team" data-backdrop="true" href="#create_team_modal" href="<?php echo base_url('index.php/team/create_team'); ?>"><span class="glyphicon glyphicon-plus"></span>Create a team</a></li> 
         </ul>
-    <li><a href="#"><i class="icon-envelope"></i>Messages</a></li>
-    <li><a href="#"><i class="icon-user"></i>My Profile</a></li>
-    <li><a href="#"><i class="icon-off"></i>Logout</a></li>
-</ul>
+    <li><a href="<?php echo base_url(); ?>index.php/message"><span class="glyphicon glyphicon-envelope"></span>Messages</a></li>
+    <li><a href="<?php echo base_url(); ?>index.php/profile"><span class="glyphicon glyphicon-user"></span></i>My Profile</a></li>
+    <li><a href="<?php echo base_url(); ?>index.php/auth/logout"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
+</ul> 
     
             <?php else : ?>
                
-<ul class="nav nav-tabs nav-stacked">
+<ul class="nav nav-pills nav-stacked">
     <li data-toggle="collapse" data-target="#test"><a href="#"><i class="icon-list"></i>My Teams</a></li>
         <ul class="nav nav-list collapse" id="test">
-            <li><a href="#">Epic United</a></li>
-            <li><a href="#">Awesome Rovers</a></li>
-            <li><a href="#">Boston Fuckups</a></li>
+            <li><a href="<?php echo base_url(); ?>">Epic United</a></li>
+            <li><a href="<?php echo base_url(); ?>">Awesome Rovers</a></li>
+            <li><a href="<?php echo base_url(); ?>">Boston Fuckups</a></li>
             <li><a data-toggle="modal" id="create_team" data-backdrop="true" href="#create_team_modal" href="<?php echo base_url('index.php/team/create_team'); ?>"><i class="icon-plus"></i>Create a team</a></li> 
         </ul>
-    <li><a href="#"><i class="icon-envelope"></i>Messages</a></li>
-    <li><a href="#"><i class="icon-user"></i>My Profile</a></li>
-    <li><a href="#"><i class="icon-off"></i>Logout</a></li>
+    <li><a href="<?php echo base_url(); ?>index.php/message"><i class="icon-envelope"></i>Messages</a></li>
+    <li><a href="<?php echo base_url(); ?>index.php/profile"><i class="icon-user"></i>My Profile</a></li>
+    <li><a href="<?php echo base_url(); ?>index.php/auth/logout"><i class="icon-off"></i>Logout</a></li>
 </ul>
             <?php endif; ?>
 
@@ -37,7 +37,7 @@
             <?php endforeach; ?>
             <?php endif; */ ?> 
         
-        <div class="modal hide fade" id="create_team_modal" keyboard="true" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal fade" id="create_team_modal" keyboard="true" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -47,44 +47,33 @@
             <div class="modal-body">
                 
                 <form id="input_form" action="<?php echo base_url('index.php/team/create_team'); ?>" method="POST">
-            
-                    <input type="text" value="<?php echo set_value('teamname'); ?>" name="teamname" placeholder="Team name" autofocus />
-                    <?php echo form_error('teamname'); ?>
-            
-                    <select name="sport" id="sport" value="">
-                        <option value="0" name="choose" selected>Choose sport</option>
-                        <option value="Football" name="Football">Football</option>
-                        <option value="Badminton" name="Badminton">Badminton</option>
-                        <option value="Bandy" name="Bandy">Bandy</option>
-                        <option value="Baseball" name="Baseball">Baseball</option>
-                        <option value="Basketball" name="Basketball">Basketball</option>
-                        <option value="Biathlon" name="Biathlon">Biathlon</option>
-                        <option value="Cross-country" name="Cross-country">Cross-country</option>
-                        <option value="Cycling" name="Cycling">Cycling</option>
-                        <option value="Field hockey" name="Field hockey">Field hockey</option>
-                        <option value="Handball" name="Handball">Handball</option>
-                        <option value="Ice hockey" name="Ice hockey">Ice hockey</option>
-                        <option value="Lacrosse" name="Lacrosse">Lacrosse</option>
-                        <option value="Rugby" name="Rugby">Rugby</option>
-                        <option value="Track and field" name="Track and field">Track and field</option>
-                        <option value="Volleyball" name="Volleyball">Volleyball</option>
-                        <option value="Water polo" name="Water polo">Water polo</option>
-                    </select>
-                    <?php echo form_error('sport'); ?>
-            
-                    <!--
-                    <select name="activity_days" id="activity_days" value="">
-                        <option value="1">Monday</option>
-                        <option value="2">Tuesday</option>
-                        <option value="3">Wednesday</option>
-                        <option value="4">Thursday</option>
-                        <option value="5">Friday</option>
-                        <option value="6">Saturday</option>
-                        <option value="7">Sunday</option>
-                    </select> 
-                    -->
-            
-                
+                    <div class="form-group">
+                        <label for="teamname">Team Name</label>
+                        <input type="text" class="form-control" value="<?php echo set_value('teamname'); ?>" name="teamname" placeholder="Team name" autofocus />
+                        <?php echo form_error('teamname'); ?>
+                        
+                        <label for="sport">Sport</label>
+                        <select class ="form-control" name="sport" id="sport" value="">
+                            <option value="0" name="choose" selected>Choose sport</option>
+                            <option value="Football" name="Football">Football</option>
+                            <option value="Badminton" name="Badminton">Badminton</option>
+                            <option value="Bandy" name="Bandy">Bandy</option>
+                            <option value="Baseball" name="Baseball">Baseball</option>
+                            <option value="Basketball" name="Basketball">Basketball</option>
+                            <option value="Biathlon" name="Biathlon">Biathlon</option>
+                            <option value="Cross-country" name="Cross-country">Cross-country</option>
+                            <option value="Cycling" name="Cycling">Cycling</option>
+                            <option value="Field hockey" name="Field hockey">Field hockey</option>
+                            <option value="Handball" name="Handball">Handball</option>
+                            <option value="Ice hockey" name="Ice hockey">Ice hockey</option>
+                            <option value="Lacrosse" name="Lacrosse">Lacrosse</option>
+                            <option value="Rugby" name="Rugby">Rugby</option>
+                            <option value="Track and field" name="Track and field">Track and field</option>
+                            <option value="Volleyball" name="Volleyball">Volleyball</option>
+                            <option value="Water polo" name="Water polo">Water polo</option>
+                        </select>
+                        <?php echo form_error('sport'); ?>
+                    </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
