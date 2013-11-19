c95e7aca7920458f9447f813061d0694eb063a86
Yngve Sundfjord <yngvesundfjord@gmail.com>
2013-11-19 14:14:54 +0100

    updated player area to match coach area

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -19,29 +19,25 @@
     
             <?php else : ?>
                
-<ul class="nav nav-pills nav-stacked">
-    <li data-toggle="collapse" data-target="#test"><a href="#"><i class="icon-list"></i>My Teams</a></li>
-        <ul class="nav nav-list collapse" id="test">
-            <li><a href="<?php echo base_url(); ?>">Epic United</a></li>
-            <li><a href="<?php echo base_url(); ?>">Awesome Rovers</a></li>
-            <li><a href="<?php echo base_url(); ?>">Boston Fuckups</a></li>
-            <li><a data-toggle="modal" id="create_team" data-backdrop="true" href="#create_team_modal" href="<?php echo base_url('index.php/team/create_team'); ?>"><i class="icon-plus"></i>Create a team</a></li> 
+<ul id="menu" class="nav nav-pills nav-stacked">
+    <li><a href="<?php echo base_url(); ?>"><span class="glyphicon glyphicon-list"></span>My Teams</a></li>
+        <ul id="submenu" class="nav">
+            <?php foreach ($plr_result as $row): ?>
+            <?php echo '<li><a href="http://localhost/master/index.php/team/' . $row['id'] . '">' .$row['teamname'] . '</a></li>';?> 
+            <?php endforeach; 
+                if(count($result) <= 2) 
+                {
+                    echo '<li><a data-toggle="modal" id="join_team" data-backdrop="true" href="#join_team_modal" href="base_url(index.php/team/join_team)"><span class="glyphicon glyphicon-plus"></span>Join a team</a></li>';
+                }
+             ?>
         </ul>
-    <li><a href="<?php echo base_url(); ?>index.php/message"><i class="icon-envelope"></i>Messages</a></li>
-    <li><a href="<?php echo base_url(); ?>index.php/profile"><i class="icon-user"></i>My Profile</a></li>
-    <li><a href="<?php echo base_url(); ?>index.php/auth/logout"><i class="icon-off"></i>Logout</a></li>
+    <li><a href="<?php echo base_url(); ?>index.php/message"><span class="glyphicon glyphicon-envelope"></span>Messages</a></li>
+    <li><a href="<?php echo base_url(); ?>index.php/profile"><span class="glyphicon glyphicon-user"></span></i>My Profile</a></li>
+    <li><a href="<?php echo base_url(); ?>index.php/auth/logout"><span class="glyphicon glyphicon-log-out"></span>Logout</a></li>
 </ul>
             <?php endif; ?>
-
-<?php /*  if (empty($result)): ?>
-            <li>Create a team</li>
-            <?php  else:  ?>
-            <?php foreach ($result as $row): ?>
-            <li><?php echo $row['teamname'];?></li>  
-            <?php endforeach; ?>
-            <?php endif; */ ?> 
         
-        <div class="modal fade" id="create_team_modal" keyboard="true" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="create_team_modal" keyboard="true" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -85,6 +81,52 @@
             </div>
             
             </form>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+
+<div class="modal fade" id="join_team_modal" keyboard="true" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h4 class="modal-title">Join team</h4>
+            </div>
+            <div class="modal-body">
+                
+                <input type="text" class="input-medium search-query" id="search_team" autocomplete="off" placeholder="Søk eller legg til..." onfocus="if
+                (this.value===this.defaultValue) this.value='';">
+    
+                <table id="team_table" class="table">
+                    <thead>
+                        <tr class="tabellheader"> 
+                            <th class="left" scope="col"><input rel="popover" class="check_all" id="selectall" type="hidden" />
+                            <th class="middle_l" scope="col">Team Name</th>
+                            <th class="middle_r" scope="col">Sport</th>
+                            <th class="right" scope="col">Coach</th>
+                        </tr>
+                    </thead>
+                    <tbody>	
+            
+                    <?php foreach ($teams as $team):?>
+                        <tr>
+                            <td class="left"><input form="team" type="checkbox" name="team[]" id="air" value="<?php echo $team['id'];?>" > </td>
+                            <td class="middle_l"><div class="teamname"><?php echo $team['teamname'];?></div></td>
+                            <td class="middle_r"><div class="sport"><?php echo $team['sport'];?></div></td>
+                            <td class="middle_r"><div class="sport"><?php echo $team['coach'];?></div></td>
+                        </tr>
+                    <?php endforeach;?>
+                    </tbody>
+                </table>
+                
+            </div>
+            <div class="modal-footer">
+                <form id="team" action="<?php echo base_url(); ?>index.php/team/join_team" method="post">
+                    <button class="btn btn-info" type="submit">Join team</button>
+                </form>
+            </div>
+            
+            
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
--- application/views/includes/template.php
+++ application/views/includes/template.php
@@ -6,11 +6,15 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="robots" content="noindex">
         <title><?php echo $title; ?></title>
-        <script src="<?php echo base_url(); ?>/js/jquery.js" type="text/javascript" charset="utf-8"></script>
-        <script src="<?php echo base_url(); ?>/js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
-        <script src="<?php echo base_url(); ?>/js/functions.js" type="text/javascript" charset="utf-8"></script>
-        <link rel="stylesheet" href="<?php echo base_url();?>/css/bootstrap.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="<?php echo base_url();?>/css/style.css" type="text/css" media="screen" />
+        <script src="<?php echo base_url(); ?>js/jquery.js" type="text/javascript" charset="utf-8"></script>
+        <script src="<?php echo base_url(); ?>js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
+        <script src="<?php echo base_url(); ?>js/jquery.dataTables.js" type="text/javascript" charset="utf-8" ></script>
+        <script src="<?php echo base_url(); ?>js/dataTables.bootstrap.js" type="text/javascript" charset="utf-8" ></script>
+        <script src="<?php echo base_url(); ?>js/functions.js" type="text/javascript" charset="utf-8"></script>
+        <link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="<?php echo base_url();?>css/jquery.dataTables.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="<?php echo base_url();?>css/dataTables.bootstrap.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" />
     </head>
     
     <body>
