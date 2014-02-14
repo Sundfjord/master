b2649f64d469e640061a0f91613aaadb91f8a8f0
Yngve Sundfjord <yngvesundfjord@gmail.com>
2014-02-14 10:31:08 +0100

    added a success div

--- application/views/home_v.php
+++ application/views/home_v.php
@@ -3,15 +3,19 @@
         <!-- HTML to display for coaches here -->
         <h2>Teams</h2>
         
+        <div id="home_success" class="success">
+        
+        </div>
+        
         <ul class="team_menu">
             <?php if (empty($coachteam)) {?>
-            <?php echo '<li><a data-toggle="modal" id="create_team" data-backdrop="true" href="#create_team_modal" href="base_url(index.php/team/create_team)"><span class="glyphicon glyphicon-plus"></span>Create a team</a></li>'; ?>
+            <?php echo '<li><a id="create_team" href="#create_team_modal"><span class="glyphicon glyphicon-plus"></span>Create a team</a></li>'; ?>
             <?php } else {?><?php foreach ($coachteam as $row): ?>
                 <?php echo '<li><a href="http://localhost/master/index.php/team/' . $row['id'] . '">' .$row['teamname'] . '</a></li>';?> 
             <?php endforeach; 
                 if(count($coachteam) <= 2) 
                 {
-                    echo '<li><a data-toggle="modal" id="create_team" data-backdrop="true" href="#create_team_modal" href="base_url(index.php/team/create_team)"><span class="glyphicon glyphicon-plus"></span>Create a team</a></li>';
+                    echo '<li><a id="create_team" href="#create_team_modal"><span class="glyphicon glyphicon-plus"></span>Create a team</a></li>';
                 }
             } ?>
         </ul>
@@ -21,16 +25,20 @@
         <h3>Player area</h3>
         <p> Welcome, <?php echo $username; ?> </p>
         
+        <div id="home_success" class="success">
+        
+        </div>
+        
         <ul class="team_menu">
             <?php if (empty($playerteam)) {?>
-            <?php echo '<li><a data-toggle="modal" id="join_team" data-backdrop="true" href="#join_team_modal" href="base_url(index.php/team/join_team)"><span class="glyphicon glyphicon-plus"></span>Join a team</a></li>';?>
+            <?php echo '<li><a id="join_team" href="#join_team_modal"><span class="glyphicon glyphicon-plus"></span>Join a team</a></li>';?>
             <?php } else {?>
             <?php foreach ($playerteam as $row): ?>
                 <?php echo '<li><a href="http://localhost/master/index.php/team/' . $row['id'] . '">' .$row['teamname'] . '</a></li>';?> 
             <?php endforeach; 
                 if(count($playerteam) <= 2) 
                 {
-                    echo '<li><a data-toggle="modal" id="join_team" data-backdrop="true" href="#join_team_modal" href="base_url(index.php/team/join_team)"><span class="glyphicon glyphicon-plus"></span>Join a team</a></li>';
+                    echo '<li><a id="join_team" href="#join_team_modal"><span class="glyphicon glyphicon-plus"></span>Join a team</a></li>';
                 }
             } ?>
         </ul>
