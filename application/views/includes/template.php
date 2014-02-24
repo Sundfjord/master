56e7be736ea9c6090648877f5be1474718a656ee
Yngve Sundfjord <yngvesundfjord@gmail.com>
2014-02-24 18:58:58 +0100

    minor

--- application/views/includes/template.php
+++ application/views/includes/template.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta name="robots" content="noindex">
-        <meta name="viewport" content="width=device-width", initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <title><?php echo $title; ?></title>
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css" />
-        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootswatch/3.0.3/journal/bootstrap.min.css" type="text/css" media="screen" />
         <link rel="stylesheet" href="<?php echo base_url();?>css/jquery.dataTables.css" type="text/css" media="screen" />
         <link rel="stylesheet" href="<?php echo base_url();?>css/dataTables.bootstrap.css" type="text/css" media="screen" />
         <link rel="stylesheet" href="<?php echo base_url();?>css/bootstrap-datepicker.css" type="text/css" media="screen" />
@@ -16,6 +14,18 @@
         <link rel="stylesheet" href="<?php echo base_url();?>css/daterangepicker-bs3.css" type="text/css" media="screen" />
         <link rel="stylesheet" href="<?php echo base_url();?>css/fullcalendar.css" type="text/css" />
         <link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen" />
+    </head>
+    
+    <body>
+        <div id="wrap">
+            <?php $this->load->view('includes/header'); ?>
+            <?php $this->load->view('includes/navbar'); ?>
+            <div id="content">
+                <?php $this->load->view($main_content); ?>
+            </div>
+            <?php $this->load->view('includes/footer'); ?>
+        </div>
+    
         <script src="<?php echo base_url(); ?>js/jquery.js" type="text/javascript" charset="utf-8"></script>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js" type="text/javascript" charset="utf-8"></script>
         <script src="<?php echo base_url(); ?>js/jquery.dataTables.js" type="text/javascript" charset="utf-8" ></script>
@@ -26,21 +36,7 @@
         <script src="<?php echo base_url(); ?>js/daterangepicker.js" type="text/javascript" charset="utf-8"></script>
         <script src="<?php echo base_url(); ?>js/fullcalendar.js" type='text/javascript' ></script> 
         <script src="<?php echo base_url(); ?>js/functions.js" type="text/javascript" charset="utf-8"></script>
-    </head>
-    
-    <body>
-        
-        <?php $this->load->view('includes/header'); ?>
-        <?php $this->load->view('includes/navbar'); ?>
-        <?php //$this->load->view('includes/sidebar'); ?>
-
-        <div id="content">
-            <?php $this->load->view($main_content); ?>
-        </div>
-
-        <?php $this->load->view('includes/footer'); ?>
-
-      
+    </body>
 </html>
         
 
