299c8a85ed9c4e42d921d72b3a9573781c21a7c9
Yngve Sundfjord <yngvesundfjord@gmail.com>
2014-02-24 18:58:54 +0100

    styled a simple temp footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,7 +1,17 @@
-<footer id="footer">
+<footer id="footer" class="footer">
     <div class="wrap container">
         <div class="footer-content row">
-            <p>This comes from the footer view</p>
+            <div class="credit col-xs-12">
+                <p>sundfjord © 2014</p>
+            </div>
+            <!--
+            <div class="col-xs-12 col-sm-6">
+                <ul>
+                    <li>About</li>
+                    <li>Support</li>
+                    <li>Contact</li>
+                </ul>
+            </div> -->
         </div><!-- .footer-content -->
     </div>
 </footer>
